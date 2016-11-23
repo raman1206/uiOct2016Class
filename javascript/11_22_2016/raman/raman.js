6045c5da7769c80b11a9bc5d4c60dc8318a9661b
Raman Meeniga Bellary <Raman Meeniga Bellary>
2016-11-22 22:24:19 -0500

    primitive data types assignment

---
+++ javascript/11_22_2016/raman/raman.js
@@ -0,0 +1,21 @@
+// primitve data types
+
+// integer type
+var a = 5;
+// float data type
+var b = 9.5;
+// string data type
+var firstname='raman';
+// null data type
+var c = null;
+// undefined data type
+var d = undefined;
+// boolean data type
+var iscssloaded = true;
+
+console.log(a);
+console.log(b);
+console.log(firstname);
+console.log(c);
+console.log(d);
+console.log(iscsscloaded);
