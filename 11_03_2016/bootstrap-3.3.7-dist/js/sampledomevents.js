4b37e5b365aa71d6f3bcfe68b27d37d7e0abbaa3
Raman Meeniga Bellary <Raman Meeniga Bellary>
2016-12-22 19:34:16 -0500

    samplejs

--- 11_03_2016/bootstrap-3.3.7-dist/js/sampledomevents.js
+++ 11_03_2016/bootstrap-3.3.7-dist/js/sampledomevents.js
@@ -4,9 +4,17 @@
                           // capturing the li tag and assigning to some variable  i.e DOM concept
 var forms = document.querySelectorAll('form');
 
+//action upon clicking form tag i.e <form onClick ="myTest()"
 window.myTest = function(){
+
    /*// to stop the default event/action
-event.PreventDefault()*/
+
+event.PreventDefault()
+
+//if you want top stop event bubbling (optional)
+
+  event.StopPropagation(); */
+
   // logic
   console.log('printed');
 }
--- 11_03_2016/bootstrap-3.3.7-dist/js/sampleeventlisteners.js
+++ 11_03_2016/bootstrap-3.3.7-dist/js/sampleeventlisteners.js
@@ -2,7 +2,7 @@
 
 (function(window){
                           // capturing the li tag and assigning to some variable  i.e DOM concept
-var forms = document.querySelectorAll('form');
+var forms = document.querySelectorAll('.form-horizontal');
  // add event listener
 
     // if you dont mention index  it will give you error because that will output in the form of array thats why we need to give index
@@ -14,5 +14,27 @@ var forms = document.querySelectorAll('form');
       console.log('stopped');
 
  }, false);
+
+
+
+   // DispatchEvent
+
+
+ // create a new event called trippleClick
+     var e = new Event('tripleClick');
+
+
+     // capture the element
+     var elem = window.document.querySelectorAll('.form-control');
+
+      // listen to the new event
+     input[0].addEventListener('tripleClick',function(event){
+         // logic to be executed
+         console.log(event);
+         console.log('Im the new custom event');
+     },false);
+
+     // register the new event to the first anchor tag
+          input[0].dispatchEvent(e);
 
 })(window);
